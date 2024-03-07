a2067f68e6d5a540e74f3aee3bae6387b694fdc6
Stijn Bogaert <stijn_bogaert@outlook.com>
2024-03-08 00:11:58 +0100

    lsp

--- init.lua
+++ init.lua
@@ -9,20 +9,17 @@ vim.cmd("set nu rnu")
 vim.g.mapleader = " "
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-
-opts = {
-}
+opts = {}
 
 require("lazy").setup("plugins")
-
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,11 @@
-function setup()
- local config = require("nvim-treesitter.configs")
+local function setup()
+	local config = require("nvim-treesitter.configs")
 
-  config.setup({
-    ensure_installed = {"rust", "lua"},
-    highlight = {enabled=true},
-    indent = {enabled = true},
-  })
- 
+	config.setup({
+		auto_install = true,
+		highlight = { enabled = true },
+		indent = { enabled = true },
+	})
 end
-return {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate",config=setup}
+
+return { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate", config = setup }
