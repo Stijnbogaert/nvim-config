50eacde084fd0a7cb5745dd4b58eea79a3e8f517
Stijn Bogaert <stijn.bogaert@arcelormittal.com>
2024-03-21 12:48:23 +0100

    more telescope mappings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -29,6 +29,8 @@ end
 local function setup()
   local builtin = require("telescope.builtin")
   vim.keymap.set('n','<C-p>',builtin.find_files,{})
+  vim.keymap.set('n','<leader>fs',builtin.grep_string,{})
+  vim.keymap.set('n','<leader>fb',builtin.buffers,{})
   vim.keymap.set('n','<leader>fg',builtin.live_grep,{})
 end
 return {
