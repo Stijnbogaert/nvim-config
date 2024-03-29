c8a244e85f80e70c3b2d2713f5f2d0f0cc023a1b
Stijn Bogaert <stijn.bogaert@arcelormittal.com>
2024-03-29 09:32:07 +0100

    lsp telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,11 +1,11 @@
 local function setup_ui_select()
--- This is your opts table
-  require("telescope").setup {
+  -- This is your opts table
+  require("telescope").setup({
     extensions = {
       ["ui-select"] = {
-        require("telescope.themes").get_dropdown {
+        require("telescope.themes").get_dropdown({
           -- even more opts
-        }
+        }),
         -- pseudo code / specification for writing custom displays, like the one
         -- for "codeactions"
         -- specific_opts = {
@@ -19,21 +19,25 @@ local function setup_ui_select()
         --      do the following
         --   codeactions = false,
         -- }
-      }
-    }
-  }
+      },
+    },
+  })
   -- To get ui-select loaded and working with telescope, you need to call
   -- load_extension, somewhere after setup function:
   require("telescope").load_extension("ui-select")
 end
 local function setup()
   local builtin = require("telescope.builtin")
-  vim.keymap.set('n','<C-p>',builtin.find_files,{})
-  vim.keymap.set('n','<leader>fs',builtin.grep_string,{})
-  vim.keymap.set('n','<leader>fb',builtin.buffers,{})
-  vim.keymap.set('n','<leader>fg',builtin.live_grep,{})
+  vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+  vim.keymap.set("n", "<leader>fs", builtin.grep_string, {})
+  vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+  vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+  vim.keymap.set("n", "<leader>fc", builtin.current_buffer_fuzzy_find, {})
+  vim.keymap.set("n", "<leader>fd", builtin.diagnostics, {})
+  vim.keymap.set("n", "<leader>fp", builtin.planets, {})
+  vim.keymap.set("n", "<leader>fr", builtin.lsp_references, {})
 end
 return {
-  { 'nvim-telescope/telescope.nvim', tag = '0.1.5', dependencies = { 'nvim-lua/plenary.nvim' }, config=setup },
-  {'nvim-telescope/telescope-ui-select.nvim',config=setup_ui_select}
+  { "nvim-telescope/telescope.nvim",           tag = "0.1.5",           dependencies = { "nvim-lua/plenary.nvim" }, config = setup },
+  { "nvim-telescope/telescope-ui-select.nvim", config = setup_ui_select },
 }
