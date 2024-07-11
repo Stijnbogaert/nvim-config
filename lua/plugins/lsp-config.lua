324da620502ce0679b076d30a31fd960c4f7390f
Stijn Bogaert <stijn_bogaert@outlook.com>
2024-07-11 13:12:28 +0200

    tab autocomplete

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -3,7 +3,7 @@ local function setup_mason()
 end
 local function setup_mason_lspconfig()
 	require("mason-lspconfig").setup({
-		ensure_installed = { "lua_ls", "marksman", "powershell_es","pyright","omnisharp","openscad_lsp" },
+		ensure_installed = { "tsserver","lua_ls", "marksman", "powershell_es","pyright","omnisharp","openscad_lsp", "rust_analyzer" },
 	})
 end
 local function setup_lspconfig()
@@ -30,6 +30,10 @@ local function setup_lspconfig()
   lspconfig.openscad_lsp.setup({
     capabilities = capabilities,
   })
+  lspconfig.rust_analyzer.setup({
+    capabilities = capabilities
+  })
+  lspconfig.tsserver.setup({capabilities=capabilities})
 	-- Global mappings.
 	-- See `:help vim.diagnostic.*` for documentation on any of the below functions
 	vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -29,6 +29,7 @@ end
 local function setup()
   local builtin = require("telescope.builtin")
   vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+  vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
   vim.keymap.set("n", "<leader>fs", builtin.grep_string, {})
   vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
   vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
