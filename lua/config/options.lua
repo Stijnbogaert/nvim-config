0d032b3725748197c1db779d7b63c2b76ae9764e
Stijn Bogaert <stijn_bogaert@outlook.com>
2025-04-15 10:19:54 +0200

    tab config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,10 +1,10 @@
-vim.wo.number= true --set current line number to the line number in the file
+vim.wo.number = true --set current line number to the line number in the file
 vim.wo.relativenumber = true --set other line numbers to relative
 
 vim.opt.expandtab = true --replace spaces with tabs
-vim.opt.tabstop=2 --spaces shown per tab
-vim.opt.softtabstop=2 --spaces applied when pressing tab
-vim.opt.shiftwidth=2
+vim.opt.tabstop = 2 --spaces shown per tab
+vim.opt.softtabstop = 2 --spaces applied when pressing tab
+vim.opt.shiftwidth = 2
 
 vim.opt.smarttab = true
 vim.opt.smartindent = true
